672a178c23bd772cd410bb4c5239ff4a9c8744e7
marjona097 <raximovamarjona097@gmail.com>
2023-01-20 12:54:04 +0500

    04

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,12 @@
 #Create a variable called 'number' and assign it the three-digit number.
-
+raqam=312
 #Find the 'number' first digit and assign to x1.
-
+x1=300
 #Find the 'number' second digit and assign to x2.
-
+x2=10
 #Find the 'number' third digit and assign to x3.
-
+x3=2
 #Create a variable called 'answer' and assign it the sum of the three digits x1, x2, x3.
-
+javob=x1+x2+x3
 #Print the value of the 'answer.
+print(x1+x2+x3)
