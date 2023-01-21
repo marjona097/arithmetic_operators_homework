aac1fd8acb0e368765a1bca50a93ed5e888fa434
marjona097 <raximovamarjona097@gmail.com>
2023-01-21 10:08:17 +0500

    06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -3,8 +3,8 @@ x=9
 #Divide the number by 2 and assign number.
 number=x/2
 #Find the fraction of the number and assign to a variable called 'answer'.
-answer=0.5
+answer=number
 #Print the answer.
-answer=4
+
 #Print the number.
-print(x/2)
+print(answer)
