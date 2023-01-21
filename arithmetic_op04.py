fa684211a495a93b2d8e854ef20f0047debb4c58
marjona097 <raximovamarjona097@gmail.com>
2023-01-21 12:38:28 +0500

    04

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,13 +1,17 @@
 #Create a variable called 'number' and assign it the three-digit number.
-raqam=111
+raqam=123
 #Find the 'number' first digit and assign to x1.
 x1=raqam//100
+print(x1)
 #Find the 'number' second digit and assign to x2.
-x2=raqam%100
+x2=(raqam%100)//10
+print(x2)
+
 x3=raqam%10
-x4=x2//10
+print(x3)
+
 #Find the 'number' third digit and assig of the three digits x1, x2, x3.
 
-number=x1+x4+x3
+number=x1+x2+x3
 #Print the value of the 'answer.
 print(number)
