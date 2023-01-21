84d0e0e55c0d70c85d8110684e6a8c31987c1d24
marjona097 <raximovamarjona097@gmail.com>
2023-01-21 09:37:22 +0500

    05

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,6 +1,7 @@
 #Create a variable called 'number' with data type in
-
+number=12
 #Create a variable called 'answer' and assign the remainder of the division of 
 # # number by 3 to it.
-
+answer=10
 #Print 'answer'.
+print(number%answer)
