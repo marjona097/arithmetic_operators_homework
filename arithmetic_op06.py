080e69cefe6a88e22febcbbaca0f36a482828946
marjona097 <raximovamarjona097@gmail.com>
2023-01-21 13:07:58 +0500

    06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,10 +1,10 @@
 #Create a variable called 'number' and assign any value of int to it.
-x=9
+number=9
 #Divide the number by 2 and assign number.
-number=x/2
+number=number//2
 #Find the fraction of the number and assign to a variable called 'answer'.
 answer=number
 #Print the answer.
-
-#Print the number.
 print(answer)
+#Print the number.
+print(number)
